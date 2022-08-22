9bd1908a30386a0c5dbf86780503b13e9de489ea
danielpdaniel <artofjquinn@gmail.com>
2022-08-22 12:46:33 -0400

    commit tree 7197cde4deb145ebe0b25ec6dc3ac5498f70990f

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.getElementById("button");
+    input.addEventListener("click", function(){
+        return alert("I was clicked!")});
 }
+
+addingEventListener()
